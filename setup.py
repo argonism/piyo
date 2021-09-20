713f423d84cc878550636c116096d87e6ea82f58
k-usuha <kouta528bado@yahoo.co.jp>
2021-09-20 20:12:26 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -22,13 +22,17 @@ setup(
 
     url='https://github.com/argonism/piyo',
 
-    description='esa API v1 client library, written in python', # パッケージの簡単な説明
-    long_description=readme(), # PyPIに'Project description'として表示されるパッケージの説明文
-    long_description_content_type='text/markdown' # long_descriptionの形式を'text/plain', 'text/x-rst', 'text/markdown'のいずれかから指定
-    keywords='gokulang goku-lang', # PyPIでの検索用キーワードをスペース区切りで指定
+    description='esa API v1 client library, written in python',
+    long_description=readme(),
+    long_description_content_type='text/markdown'
+    keywords='esa api client esa.io python',
 
     classifiers=[
         'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-    ], # パッケージ(プロジェクト)の分類。https://pypi.org/classifiers/に掲載されているものを指定可能。
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
 )
