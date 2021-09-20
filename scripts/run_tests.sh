635369936ecd5f11edb8d3654e6f13a18e05c054
k-usuha <kouta528bado@yahoo.co.jp>
2021-09-21 03:20:04 +0900

    update run_tests script

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -4,12 +4,9 @@ SCRIPT_DIR=`dirname $0`
 PROJECT_DIR=$SCRIPT_DIR/..
 
 cd $PROJECT_DIR
-# TEST_FILES=tests/test_*
-# for file_path in $TEST_FILES; do
-#     FILENAME=`basename $file_path`
-#     echo "\n\ntest: $FILENAME\n"
-#     python3 -m unittest $file_path -v
-# done
-
-# python3 -m unittest tests/test_* -v
-python3 tests/test_team_endpoint.py -v
+TEST_FILES=tests/test_*
+for file_path in $TEST_FILES; do
+    FILENAME=`basename $file_path`
+    echo "\n\ntest: $FILENAME\n"
+    python3 $file_path -v
+done
