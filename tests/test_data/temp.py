ff14862edccb1981073a2c47f81de56ed542cbd5
k-usuha <kouta528bado@yahoo.co.jp>
2021-09-26 18:04:22 +0900

    add test_data

---
+++ tests/test_data/temp.py
@@ -0,0 +1,16 @@
+import os, re
+with open("workfile.txt") as f:
+    dataset = f.read().split("-------------------------------------------------------\n")
+
+for data in dataset:
+    http_line = data.split("\n")[0]
+    method, path, ver = http_line.strip().split(" ")
+    filename = f"{method.lower()}_{'_'.join(path.split('/')[2:])}.txt"
+    splitted = [ e for e in re.split(r"\n\n", data) if e ]
+    with open(filename, "w", newline="") as f:
+        res_body = splitted[-1].rstrip()
+        if res_body == "HTTP/1.1 204 No Content":
+            res_body = ""
+        f.write(http_line)
+        f.write("\n")
+        f.write(res_body)
