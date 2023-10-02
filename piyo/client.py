67ae86090b25aecbaaf327a9322f303cbf6f6aa6
k-usuha <kouta528bado@yahoo.co.jp>
2023-10-03 01:53:19 +0900

    Refactor request base function

--- piyo/client.py
+++ piyo/client.py
@@ -1,4 +1,5 @@
 import requests, os, json
+from pathlib import Path
 from enum import Enum, auto
 from .error import PiyoException, PiyoEmptyTeamException, PiyoNotImplementedException, PiyoHTTPException
 
@@ -17,9 +18,7 @@ class Client(object):
     def _credential_header(self, access_token):
         return {"Authorization": "Bearer {0}".format(access_token)}
 
-    def _request(self, method, path, params={}, headers={}, data={}, with_auth=True):
-        if with_auth: headers = dict(self._credential_header(self.access_token), **headers)
-        url = "{0}{1}".format(self.api_endpoint, path)
+    def _request(self, method, url, params={}, headers={}, data={}):
         response = None
         try:
             if method == RequestMethod.GET:
@@ -51,6 +50,11 @@ class Client(object):
             )
 
         return results
+
+    def _request_to_esa(self, method, path, params={}, headers={}, data={}, with_auth=True):
+        if with_auth: headers = dict(self._credential_header(self.access_token), **headers)
+        url = "{0}{1}".format(self.api_endpoint, path)
+        return self._request(method, url, params=params, headers=headers, data=data)
     
     def team_required(func):
         def wrapper(self, *args, **kwargs):
@@ -65,27 +69,27 @@ class Client(object):
 
     def teams(self, params={}, headers={}):
         path = "/v1/teams"
-        return self._request(RequestMethod.GET, path, params, headers)
+        return self._request_to_esa(RequestMethod.GET, path, params, headers)
 
     @team_required
     def team(self, params={}, headers={}):
         path = "/v1/teams/{0}".format(self.current_team)
-        return self._request(RequestMethod.GET, path, params, headers)
+        return self._request_to_esa(RequestMethod.GET, path, params, headers)
 
     @team_required
     def stats(self, params={}, headers={}):
         path = "/v1/teams/{0}/stats".format(self.current_team)
-        return self._request(RequestMethod.GET, path, params, headers)
+        return self._request_to_esa(RequestMethod.GET, path, params, headers)
 
     @team_required
     def members(self, params={}, headers={}):
         path = "/v1/teams/{0}/members".format(self.current_team)
-        return self._request(RequestMethod.GET, path, params, headers)
+        return self._request_to_esa(RequestMethod.GET, path, params, headers)
 
     @team_required
     def delete_member(self, screen_name, params={}, headers={}):
         path = "/v1/teams/{0}/members/{1}".format(self.current_team, screen_name)
-        return self._request(RequestMethod.DELETE, path, params, headers)
+        return self._request_to_esa(RequestMethod.DELETE, path, params, headers)
 
     @team_required
     def posts(self, keywords=[], search_options={}, params={}, headers={}):
@@ -94,27 +98,27 @@ class Client(object):
             options = [f"{str(k)}: {str(v)}" for k, v in search_options.items()]
             q_param = " ".join([str(k) for k in keywords] + options)
             params["q"] = q_param
-        return self._request(RequestMethod.GET, path, params, headers)
+        return self._request_to_esa(RequestMethod.GET, path, params, headers)
 
     @team_required
     def post(self, post_number, params={}, headers={}):
         path = "/v1/teams/{0}/posts/{1}".format(self.current_team, post_number)
-        return self._request(RequestMethod.GET, path, params, headers)
+        return self._request_to_esa(RequestMethod.GET, path, params, headers)
     
     @team_required
     def create_post(self, data, params={}, headers={}):
         path = "/v1/teams/{0}/posts".format(self.current_team)
-        return self._request(RequestMethod.POST, path, params, headers, data=data)
+        return self._request_to_esa(RequestMethod.POST, path, params, headers, data=data)
     
     @team_required
     def update_post(self, post_number, data, params={}, headers={}):
         path = "/v1/teams/{0}/posts/{1}".format(self.current_team, post_number)
-        return self._request(RequestMethod.PATCH, path, params, headers, data=data)
+        return self._request_to_esa(RequestMethod.PATCH, path, params, headers, data=data)
     
     @team_required
     def delete_post(self, post_number, params={}, headers={}):
         path = "/v1/teams/{0}/posts/{1}".format(self.current_team, post_number)
-        return self._request(RequestMethod.DELETE, path, params, headers)
+        return self._request_to_esa(RequestMethod.DELETE, path, params, headers)
 
     @team_required
     def comments(self, post_number=None, params={}, headers={}):
@@ -122,121 +126,121 @@ class Client(object):
             path = "/v1/teams/{0}/posts/{1}/comments".format(self.current_team, post_number)
         else:
             path = "/v1/teams/{0}/comments".format(self.current_team)
-        return self._request(RequestMethod.GET, path, params, headers)
+        return self._request_to_esa(RequestMethod.GET, path, params, headers)
 
     @team_required
     def comment(self, comment_id, params={}, headers={}):
         path = "/v1/teams/{0}/comments/{1}".format(self.current_team, comment_id)
-        return self._request(RequestMethod.GET, path, params, headers)
+        return self._request_to_esa(RequestMethod.GET, path, params, headers)
 
     @team_required
     def create_comment(self, post_number, data, params={}, headers={}):
         path = "/v1/teams/{0}/posts/{1}/comments".format(self.current_team, post_number)
-        return self._request(RequestMethod.POST, path, params, headers, data=data)
+        return self._request_to_esa(RequestMethod.POST, path, params, headers, data=data)
 
     @team_required
     def update_comment(self, comment_id, data, params={}, headers={}):
         path = "/v1/teams/{0}/comments/{1}".format(self.current_team, comment_id)
-        return self._request(RequestMethod.PATCH, path, params, headers, data=data)
+        return self._request_to_esa(RequestMethod.PATCH, path, params, headers, data=data)
 
     @team_required
     def delete_comment(self, comment_id, params={}, headers={}):
         path = "/v1/teams/{0}/comments/{1}".format(self.current_team, comment_id)
-        return self._request(RequestMethod.DELETE, path, params, headers)
+        return self._request_to_esa(RequestMethod.DELETE, path, params, headers)
 
     @team_required
     def post_stargazers(self, post_number, params={}, headers={}):
         path = "/v1/teams/{0}/posts/{1}/stargazers".format(self.current_team, post_number)
-        return self._request(RequestMethod.GET, path, params, headers)
+        return self._request_to_esa(RequestMethod.GET, path, params, headers)
 
     @team_required
     def add_post_star(self, post_number, data={}, params={}, headers={}):
         path = "/v1/teams/{0}/posts/{1}/star".format(self.current_team, post_number)
-        return self._request(RequestMethod.POST, path, params, headers, data=data)
+        return self._request_to_esa(RequestMethod.POST, path, params, headers, data=data)
 
     @team_required
     def delete_post_star(self, post_number, params={}, headers={}):
         path = "/v1/teams/{0}/posts/{1}/star".format(self.current_team, post_number)
-        return self._request(RequestMethod.DELETE, path, params, headers)    
+        return self._request_to_esa(RequestMethod.DELETE, path, params, headers)    
 
     @team_required
     def comment_stargazers(self, comment_id, params={}, headers={}):
         path = "/v1/teams/{0}/comments/{1}/stargazers".format(self.current_team, comment_id)
-        return self._request(RequestMethod.GET, path, params, headers)
+        return self._request_to_esa(RequestMethod.GET, path, params, headers)
 
     @team_required
     def add_comment_star(self, comment_id, data={}, params={}, headers={}):
         path = "/v1/teams/{0}/comments/{1}/star".format(self.current_team, comment_id)
-        return self._request(RequestMethod.POST, path, params, headers, data=data)
+        return self._request_to_esa(RequestMethod.POST, path, params, headers, data=data)
 
     @team_required
     def delete_comment_star(self, comment_id, params={}, headers={}):
         path = "/v1/teams/{0}/comments/{1}/star".format(self.current_team, comment_id)
-        return self._request(RequestMethod.DELETE, path, params, headers)
+        return self._request_to_esa(RequestMethod.DELETE, path, params, headers)
 
     @team_required
     def watchers(self, post_number, params={}, headers={}):
         path = "/v1/teams/{0}/posts/{1}/watchers".format(self.current_team, post_number)
-        return self._request(RequestMethod.GET, path, params, headers)
+        return self._request_to_esa(RequestMethod.GET, path, params, headers)
 
     @team_required
     def add_watch(self, post_number, params={}, headers={}):
         path = "/v1/teams/{0}/posts/{1}/watch".format(self.current_team, post_number)
-        return self._request(RequestMethod.POST, path, params, headers)
+        return self._request_to_esa(RequestMethod.POST, path, params, headers)
 
     @team_required
     def delete_watch(self, post_number, params={}, headers={}):
         path = "/v1/teams/{0}/posts/{1}/watch".format(self.current_team, post_number)
-        return self._request(RequestMethod.DELETE, path, params, headers)
+        return self._request_to_esa(RequestMethod.DELETE, path, params, headers)
 
     @team_required
     def batch_move(self, data, params={}, headers={}):
         path = "/v1/teams/{0}/categories/batch_move".format(self.current_team)
-        return self._request(RequestMethod.POST, path, params, headers)
+        return self._request_to_esa(RequestMethod.POST, path, params, headers)
 
     @team_required
     def invitation(self, params={}, headers={}):
         path = "/v1/teams/{0}/invitation".format(self.current_team)
-        return self._request(RequestMethod.GET, path, params, headers)
+        return self._request_to_esa(RequestMethod.GET, path, params, headers)
 
     @team_required
     def regenerate_invitation(self, params={}, headers={}):
         path = "/v1/teams/{0}/invitation_regenerator".format(self.current_team)
-        return self._request(RequestMethod.POST, path, params, headers)
+        return self._request_to_esa(RequestMethod.POST, path, params, headers)
 
     @team_required
     def send_invitation(self, data, params={}, headers={}):
         path = "/v1/teams/{0}/invitations".format(self.current_team)
-        return self._request(RequestMethod.POST, path, params, headers, data=data)
+        return self._request_to_esa(RequestMethod.POST, path, params, headers, data=data)
 
     @team_required
     def delete_invitation(self, code, params={}, headers={}):
         path = "/v1/teams/{0}/invitations/{1}".format(self.current_team, code)
-        return self._request(RequestMethod.DELETE, path, params, headers)
+        return self._request_to_esa(RequestMethod.DELETE, path, params, headers)
 
     @team_required
     def invitations(self, params={}, headers={}):
         path = "/v1/teams/{0}/invitations".format(self.current_team)
-        return self._request(RequestMethod.GET, path, params, headers)
+        return self._request_to_esa(RequestMethod.GET, path, params, headers)
 
     @team_required
     def emojis(self, params={}, headers={}):
         path = "/v1/teams/{0}/emojis".format(self.current_team)
-        return self._request(RequestMethod.GET, path, params, headers)
+        return self._request_to_esa(RequestMethod.GET, path, params, headers)
 
     @team_required
     def create_emoji(self, data, params={}, headers={}):
         path = "/v1/teams/{0}/emojis".format(self.current_team)
-        return self._request(RequestMethod.POST, path, params, headers)
+        return self._request_to_esa(RequestMethod.POST, path, params, headers)
 
     @team_required
     def delete_emoji(self, code, params={}, headers={}):
         path = "/v1/teams/{0}/emojis/{1}".format(self.current_team, code)
-        return self._request(RequestMethod.DELETE, path, params, headers)
+        return self._request_to_esa(RequestMethod.DELETE, path, params, headers)
 
     def user(self, params={}, headers={}):
         path = "/v1/user"
-        return self._request(RequestMethod.GET, path, params, headers)
+        return self._request_to_esa(RequestMethod.GET, path, params, headers)
 
 
 if __name__ == "__main__":
